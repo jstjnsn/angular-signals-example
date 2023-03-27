4f053efd2fc4993fc5603623fc44ac34f6732ceb
Joost Jansen <joost.jansen1@ns.nl>
2023-03-27 11:14:29 +0200

    Set up angular 16.0.0-next.3 with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-signals';
+  search = signal('');
+
+  users = signal([
+    {
+      id: 1,
+      name: 'John'
+    },
+    {
+      id: 2,
+      name: 'Mary'
+    },
+    {
+      id: 3,
+      name: 'Jon'
+    },
+  ])
+
+  filteredUsers = computed(() => this.users().filter(u => u.name.toLowerCase().indexOf(this.search().toLowerCase()) > -1))
+
+  logger = effect(() => {
+    console.log(this.search())
+  })
+
+  setSearchTerm(event: Event) {
+    this.search.set((event.target as HTMLInputElement).value);
+  }
 }
